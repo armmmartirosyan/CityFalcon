5c8fe4e5f9ba45c2549337e9f120dc7585701ebe
armmmartirosyan <armen.martirosyan020@gmail.com>
2022-10-11 20:02:44 +0400

    addeed filters with styles

---
+++ src/pages/CityFalcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from "../components/Wrapper";
+
+function WatchList() {
+    return (
+        <Wrapper className='wrapper'>
+
+        </Wrapper>
+    );
+}
+
+export default WatchList;
